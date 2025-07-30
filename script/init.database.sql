910f2053db818c152f56ae986d3fe87d0c16895e
Prathyu-Alekhya <prathyusharaparthi1998@gmail.com>
2025-07-30 16:23:25 -0500

    Update init.database.sql

--- script/init.database.sql
+++ script/init.database.sql
@@ -1,3 +1,14 @@
+/*
+=======================================
+Create Database and Schemas
+=======================================
+Script Purpose:
+	This script creates a new database named 'DataWareHouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas within the database:'Bronze','silver', and 'Gold'
+Warning:
+	Running this script will drop the entire 'DataWareHouse' database if it exists.
+	All data in the database will be permanently deleted. Proceed with the caution and ensure you have proper backups b4 running this script.
+
 use master;
 Go
 
